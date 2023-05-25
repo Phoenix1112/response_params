a8e15ddf52fde37101ffc756ef20653bb7ef937d
Phoenix1112 <42475284+Phoenix1112@users.noreply.github.com>
2023-05-25 13:56:46 +0300

    Update ValueFinder.py

--- ValueFinder.py
+++ ValueFinder.py
@@ -8,7 +8,7 @@ from colorama import *
 from concurrent.futures import ThreadPoolExecutor
 
 requests.packages.urllib3.disable_warnings()
-
+init(autoreset=True)
 def req(url):
 
 	try:
@@ -58,29 +58,52 @@ def req(url):
 
 		if len(reg3) > 0:
 
-			with print_lock:
-				print("""
+			if not args.combine:
+				with print_lock:
+					print("""
 
-					\r{}
-					\r{}
-					\r{}
+						\r{}
+						\r{}
+						\r{}
 
 
-					""".format(Fore.YELLOW+"#"*len(url), Fore.MAGENTA+str(url), Fore.YELLOW+"#"*len(url)))
+						""".format(Fore.YELLOW+"#"*len(url), Fore.MAGENTA+str(url), Fore.YELLOW+"#"*len(url)))
 
-			if args.output:
-				print_now(url,"çift")
+				if args.output:
+					print_now(url,"çift")
 
 
 			for i in reg3:
 
 				i = i.replace("'","").replace('"','').replace(" +","").replace("+ ","").replace(" +","").replace(" ","")
 
-				with print_lock:
-					print(Fore.GREEN + f"{i}")
+				if args.combine:
+					if "?" in url:
+						if not i + "=" in url:
+							i = url + "&" + i + "=1"
+						else:
+							pass
+					else:
+						if url.endswith("/"):
+							i = url + "?" + i + "=1"
+						else:
+							if not url.endswith(".jpg") and not url.endswith(".png") and not url.endswith(".svg"):
+								if not url.endswith(".html") and not url.endswith(".php") and not url.endswith(".asp") and not url.endswith(".aspx"):
+									i = url + "/?" + i + "=1"
+								else:
+									i = url + "?" + i + "=1"
 
-				if args.output:
-					print_now(i,"tek")
+					with print_lock:
+						print(Fore.GREEN + f"{i}")
+
+					if args.output:
+						print_now(i,"tek")
+				else:
+					with print_lock:
+						print(Fore.GREEN + f"{i}")
+
+					if args.output:
+						print_now(i,"tek")
 
 	except Exception as e:
 		print(e)
@@ -111,6 +134,7 @@ if __name__ == "__main__":
 	ap = argparse.ArgumentParser()
 	ap.add_argument("--url", metavar="", required=False, help="Target URL")
 	ap.add_argument("--list", metavar="", required=False, help="Target URL List")
+	ap.add_argument("--combine", required=False, action="store_true", help="Combine parameters with url.")
 	ap.add_argument("--redirect", required=False, action="store_true", help="Follow Redirects")
 	ap.add_argument("--output", metavar="", required=False, help="save output")
 	ap.add_argument("--time-out", metavar="", required=False, default=10, type=int,help="Request Timeout(Default-10)")
